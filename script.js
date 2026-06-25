ff9de8c8e5b70e853d31f44e3e408d8bbd8397c5
aaryandevwork <aaryandevwork@gmail.com>
2026-06-25 18:27:50 +0530

    some ui problems solved

--- script.js
+++ script.js
@@ -269,6 +269,7 @@ allTask.addEventListener("click", () => {
 
     todaybtn.classList.remove('active');
     importantBtn.classList.remove('active');
+    Completedbtn.classList.remove('active');
 
     renderTask();
 })
@@ -297,6 +298,7 @@ todaybtn.addEventListener("click", () => {
 
     importantBtn.classList.remove('active');
     allTask.classList.remove('active');
+    Completedbtn.classList.remove('active');
 
     renderTask(filteredTasks);
 });
@@ -370,6 +372,41 @@ let addStatistics = () => {
 }
 
 addStatistics();
+
+
+// theme toggle
+const themeBtn = document.querySelector(".theme-toggle");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-theme");
+    // document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("light-theme")) {
+        localStorage.setItem("theme", "light");
+        themeBtn.innerHTML = `
+        <span>Mode</span>
+        <i class="ri-moon-fill"></i>
+        `;
+    } else {
+        localStorage.setItem("theme", "dark");
+        themeBtn.innerHTML = `
+            <span>Mode</span>
+            <i class="ri-sun-fill"></i>
+        `;
+    }
+});
+
+
+//for page refresh
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+    document.body.classList.add("light-theme");
+} else {
+    document.body.classList.add("dark-theme");
+}
+
 
 // initial render 
 renderTask();
